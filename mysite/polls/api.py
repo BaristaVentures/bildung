b2cc39f85c22250ac089b33cee4973718d1e0781
Juan Diego Estrada <jdestradap@gmail.com>
2012-08-01 21:13:45 -0500

    playing withthe Poll json

--- mysite/polls/api.py
+++ mysite/polls/api.py
@@ -1,8 +1,17 @@
 from tastypie.resources import ModelResource
 from polls.models import Choice, Poll
+from tastypie import fields
 
 
 class PollResource(ModelResource):
     class Meta:
         queryset = Poll.objects.all()
-        # resource_name = 'entry'
+        # resource_name = 'poll'
+        # authorization= Authorization()
+
+class ChoiceResource(ModelResource):
+    poll = fields.ForeignKey(PollResource, 'poll')
+
+    class Meta:
+        queryset = Choice.objects.all()
+        # resource_name = 'choice'
