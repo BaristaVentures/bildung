a8560c3e78932f16734115c3b00b811ab9bf2ddc
Juan Diego Estrada <jdestradap@gmail.com>
2012-08-01 20:06:05 -0500

    playing withthe Poll json

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+poll_resource = PollResource()
+
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
