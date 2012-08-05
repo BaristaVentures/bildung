813028e9ecca8225f83d5b6229ba983675a538ec
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-05 04:24:46 -0400

    Added first view; index.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,7 +106,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "~/NullIndustries/Projects/bildung/mysite/mytemplates/admin"
+    "../mysite/mytemplates/admin",
+    "../mysite/mytemplates/",
 )
 
 INSTALLED_APPS = (
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^polls/$', 'polls.views.index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.results'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
     url(r'^admin/', include(admin.site.urls)),
 )
