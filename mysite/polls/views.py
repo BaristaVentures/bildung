4179c7a9bbc4845943ddfec7bad425453791eeb0
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-06 12:22:29 -0400

    Modified views and urls.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,18 +4,18 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from polls.models import Choice, Poll
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', { 'latest_poll_list': latest_poll_list })
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     return render_to_response('polls/index.html', { 'latest_poll_list': latest_poll_list })
 
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', { 'poll': p },
-                              context_instance=RequestContext(request))
+# def detail(request, poll_id):
+#     p = get_object_or_404(Poll, pk=poll_id)
+#     return render_to_response('polls/detail.html', { 'poll': p },
+#                               context_instance=RequestContext(request))
 
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/results.html', {'poll': p})
+# def results(request, poll_id):
+#     p = get_object_or_404(Poll, pk=poll_id)
+#     return render_to_response('polls/results.html', {'poll': p})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
@@ -33,4 +33,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
