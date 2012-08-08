c0855a94bc0329a4e08d3629f586b7878045a1b0
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-08 12:20:58 -0400

    Added heroku db configuration.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,3 +1,4 @@
+import dj_databse_url
 # Django settings for mysite project.
 
 DEBUG = True
@@ -10,14 +11,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mysitedb',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_databse_url.config(default='postgres://localhost')  # {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'mysitedb',                      # Or path to database file if using sqlite3.
+    #     'USER': '',                      # Not used with sqlite3.
+    #     'PASSWORD': '',                  # Not used with sqlite3.
+    #     'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    # }
 }
 
 # Local time zone for this installation. Choices can be found here:
