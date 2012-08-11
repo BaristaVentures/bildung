e8994e9711badcc9e59e616a568146a2113e7b11
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-11 16:13:00 -0400

    Fixed minor bug on templates routes.

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,8 +110,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "../mysite/mytemplates/admin",
-    "../mysite/mytemplates/",
+    "mytemplates/admin",
+    "mytemplates/",
 )
 
 INSTALLED_APPS = (
