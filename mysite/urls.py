3a707a9f835243ef60e8d80c6876579a61c2e24c
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-11 17:42:59 -0400

    Added choices to the api.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
-from polls.api import PollResource
+from polls.api import PollResource, ChoiceResource
 
 poll_resource = PollResource()
+choice_resource = ChoiceResource()
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^api/', include(poll_resource.urls)),
+    (r'^api/', include(choice_resource.urls)),
 )
--- polls/api.py
+++ polls/api.py
@@ -1,8 +1,18 @@
+from tastypie.authorization import Authorization
+from tastypie import fields
 from tastypie.resources import ModelResource
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 
 class PollResource(ModelResource):
     class Meta:
         queryset = Poll.objects.all()
         resource_name = 'polls'
+        authorization = Authorization()
+
+class ChoiceResource(ModelResource):
+    poll = fields.ForeignKey(PollResource, 'poll')
+    class Meta:
+        queryset = Choice.objects.all()
+        resource_name = 'choices'
+        authorization = Authorization()
