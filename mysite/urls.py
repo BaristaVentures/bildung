b419ab613130dad41b1c16757e283b4869b5b8fe
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-11 17:49:34 -0400

    Tidyed api's structure

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
+from tastypie.api import Api
 from polls.api import PollResource, ChoiceResource
 
-poll_resource = PollResource()
-choice_resource = ChoiceResource()
+v1_api = Api(api_name='v1')
+v1_api.register(PollResource())
+v1_api.register(ChoiceResource())
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +14,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^api/', include(poll_resource.urls)),
-    (r'^api/', include(choice_resource.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
