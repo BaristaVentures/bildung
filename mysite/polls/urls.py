fd156f230a63f7a003db7a9f0afb0fc6479da8aa
Esteban Angee <esteban.angee@gmail.com>
2012-08-20 13:06:40 -0500

    finished part 3 of django poll tutorial

---
+++ mysite/polls/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('polls.views',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from polls.models import Poll
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+  latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+  return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+  p = get_object_or_404(Poll, pk=poll_id)
+  return render_to_response('polls/detail.html', {'poll': p})
+
+def results(request, poll_id):
+  return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+  return HttpResponse("You're voting on poll %s." % poll_id)
