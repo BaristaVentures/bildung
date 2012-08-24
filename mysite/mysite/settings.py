7a9c12a78b45996092d2a224e757d8b5e16ab616
Achury <achuryds@gmail.com>
2012-08-24 13:40:30 -0500

    Fixing path problems

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add postgresql_psycopg2, mysql, sqlite3 or oracle.
-        'NAME': '/home/santiago/Documents/mysite/sqlite3.db', # Or path to database file if using sqlite3.
+        'NAME': '/home/santiago/Documents/NullIndustries/bildung/mysite/sqlite3.db', # Or path to database file if using sqlite3.
         'USER': 'achuryds',                      # Not used with sqlite3.
         'PASSWORD': 'daniels',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
