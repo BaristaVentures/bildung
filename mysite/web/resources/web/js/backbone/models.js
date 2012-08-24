7d567c5bacb69dbb63d0ebaad0ee96198abf0b56
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-08-24 17:45:07 -0400

    Code polished.

---
+++ mysite/web/resources/web/js/backbone/models.js
@@ -0,0 +1,44 @@
+/*
+ **************************************************************
+ * This section controls the polls model.
+ **************************************************************
+ */
+var Polls = Backbone.Collection.extend({
+    model: Poll,
+    url: "/api/v1/polls/",
+    parse: function(data){
+	return data.objects;
+    }
+});
+
+/*
+ **************************************************************
+ * This section controls the poll model.
+ **************************************************************
+ */
+var Poll = Backbone.Model.extend({
+    urlRoot: "/api/v1/polls/",
+});
+
+
+/*
+ **************************************************************
+ * This section controls the choices model.
+ **************************************************************
+ */
+var Choices = Backbone.Collection.extend({
+    model: Choice,
+    url: "/api/v1/choices/",
+    parse: function(data){
+	return data.objects;
+    }
+});
+
+/*
+ **************************************************************
+ * This section controls the choice model.
+ **************************************************************
+ */
+var Choice = Backbone.Model.extend({
+    urlRoot: "/api/v1/choices/",
+});
