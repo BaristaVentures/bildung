5573b3c5041422d345d89822898c6b9bb58f999d
Achury <achuryds@gmail.com>
2012-08-28 14:02:02 -0500

    Tutorial Finished

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,26 +5,14 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list' : latest_poll_list})
 
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk = poll_id)
-    return render_to_response('polls/detail.html', {'poll' : p}, 
-                              context_instance = RequestContext(request))
-
-def results(request, poll_id):
+def vote(request, poll_id):
     p = get_object_or_404(Poll, pk = poll_id)
     try:
         selected_choice = p.choice_set.get(pk = request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render_to_response('polls/detail.html', {'poll' : p, 'error_message' : "You didn't select a choice",}, context_instance = ResquestContext(request))
+        return render_to_response('polls/detail.html', {'poll' : p, 'error_message' : "You didn't select a choice",}, context_instance = RequestContext(request))
     else: 
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls.views.results', args = (p.id,)))
-    
-
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." %  poll_id)
+        return HttpResponseRedirect(reverse('poll_results', args = (p.id,)))
