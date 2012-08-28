fec7ac9feaca1fb6a960f2d03e232397486e344f
Achury <achuryds@gmail.com>
2012-08-28 02:13:32 -0500

    Improving the views and urls stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
 
-    '/home/santiago/Documents/NullIndustries/bildung/mysite/templates/admin'
+    '/home/santiago/Documents/NullIndustries/bildung/mysite/templates/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
+                  
 )
