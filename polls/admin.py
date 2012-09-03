027a69c07396495b54921215983d678c9dc7cac7
Sergio Botero <sergiobuj@gmail.com>
2012-09-03 01:18:02 -0500

    Bring more tools to admin site
    Just finished #customize-the-admin-change-list from the poll intro tutorial
    and now ready to start with #customize-the-admin-look-and-feel.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,20 @@
-from polls.models import Poll
+from polls.models import Poll, Choice
 from django.contrib import admin
 
-admin.site.register(Poll)
+class choiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['question']}),
+        ('Date information', {'fields' : ['pub_date'], 'classes' : ['collapse']}),
+    ]
+    inlines = [choiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,9 @@ class Poll(models.Model):
         return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
