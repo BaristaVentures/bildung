e02ed970e4bba523f8fee2a941f11de9b1d80c0a
Esteban Angee <esteban.angee@gmail.com>
2012-09-04 11:14:37 -0500

    created full working API to poll application

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
-from polls.api.resources.api import PollResource
+from polls.api.resources.api import PollResource, ChoiceResource
+from tastypie.api import Api
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-poll_resource = PollResource()
+v1_api = Api(api_name='v1')
+v1_api.register(PollResource())
+v1_api.register(ChoiceResource())
 
 urlpatterns = patterns('',
-    (r'^api/', include(poll_resource.urls)),
+    url(r'^polls/', include('polls.urls')),    
     url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
