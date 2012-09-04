d01e25642c8fc54f471d4fdd48b97b9e3e01ac48
Sergio Botero <sergiobuj@gmail.com>
2012-09-03 23:21:42 -0500

    Decouple URLconf
    Moved specific poll urls to the polls folder.

--- pollsweb/urls.py
+++ pollsweb/urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
-    url(r'^polls/$', 'index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
-)
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
