c697d2d8d4a1432ee74ddfdd1a8929a7d0ea16e6
Sergio Botero <sergiobuj@gmail.com>
2012-09-04 00:39:49 -0500

    Finish polls tutorial from dj web
    Modified the template to match the predefined name for the poll_list
    and using now the generic views method while using custom views.

--- polls/views.py
+++ polls/views.py
@@ -4,18 +4,6 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from polls.models import Poll, Choice
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk = poll_id)
-    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
-
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk = poll_id)
-    return render_to_response('polls/results.html', {'poll' : p})
-
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
@@ -28,4 +16,4 @@ def vote(request, poll_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
--- pollsweb/settings.py
+++ pollsweb/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-'''
+#'''
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 '''
@@ -23,7 +23,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-#'''
+'''
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
