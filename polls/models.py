1f95e11c16af5b23b1c24a2df3f93f1122c1358c
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-09-05 09:40:25 -0400

    Added extra information to models and tidied code up.

--- polls/models.py
+++ polls/models.py
@@ -5,31 +5,41 @@ from django.utils import timezone
 # Create your models here.
 
 class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question = models.CharField("poll's question", max_length=200)
+    pub_date = models.DateTimeField("date published")
 
     def __unicode__(self):
-        return self.question
+        poll = { 
+            'Question': self.question, 
+            'Pub Date': self.pub_date 
+        }
+        return str(poll)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
+    poll = models.ForeignKey(Poll, verbose_name="related poll")
+    choice = models.CharField("choice's name", max_length=200)
+    votes = models.IntegerField("choices's votes")
 
     def __unicode__(self):
-        return self.choice
+        choice = {
+            'Poll': self.poll.id,
+            'Choice': self.choice,
+            'Votes': self.votes
+        }
+        return str(choice)
 
 class Comment(models.Model):
-    poll = models.ForeignKey(Poll)
-    author = models.CharField(max_length=20)
-    comment = models.TextField(max_length=1000)
-    pub_date = models.DateTimeField('date published')
+    poll = models.ForeignKey(Poll, verbose_name="related poll")
+    author = models.CharField("comment's author", max_length=20)
+    comment = models.TextField("comment's body", max_length=1000)
+    pub_date = models.DateTimeField("date published")
     
     def __unicode__(self):
         comment = {
