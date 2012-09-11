319920c0dabe8b52f073aa47fd486745732ce251
Esteban Angee <esteban.angee@gmail.com>
2012-09-11 08:54:42 -0500

    fixed DOM loading bug, the poll index is working without choices

--- backbone_client/app.js
+++ backbone_client/app.js
@@ -1,24 +1,34 @@
 window.Poll = Backbone.Model.extend({
     url: POLLS_API,
-  /*  
-  url: function(){
-     return this.get('resource_uri') || this.collection.url;
-  }
-  */
+    
+    
+
 });
 
-window.Polls = Backbone.Collection.extend({
+window.PollList = Backbone.Collection.extend({
   url: POLLS_API,
   model: Poll, 
-  parse: function(data){
-      return data.objects;
-  }                                             
+
+  getCurrentDate: function(){
+        var currentDate =  new Date();
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var seconds = currentDate.getSeconds();
+        return hours + ":" + minutes + ":" + seconds    
+  },  
+
 });
 
 window.PollView = Backbone.View.extend({
   tagName: 'li',
   className: 'poll',
- 
+
+    /*
+  initialize: function() {
+      this.model.on('change', this.render, this);
+      this.model.on('destroy', this.remove, this);
+  },*/
+
   render: function(){
       $(this.el).html(ich.pollTemplate(this.model.toJSON()));
       return this;
@@ -27,21 +37,22 @@ window.PollView = Backbone.View.extend({
 
 
 window.App = Backbone.View.extend({
-  el: $("#app"),
+  el: $('#app'),
  
   events: {
-      "click .create" : "createPoll",
+      'click .create' : 'createPoll',
   },
  
   initialize: function(){
       _.bindAll(this, 'addOne', 'addAll', 'render');
-      this.polls = new Polls();
+      this.polls = new PollList();
       this.polls.bind('add', this.addOne);
-      this.polls.bind('refresh', this.addAll);
+      this.polls.bind('reset', this.addAll, this.view);
       this.polls.bind('all', this.render);
       this.polls.fetch();
   },
  
+    
   addAll: function(){
       this.polls.each(this.addOne);
   },
@@ -57,11 +68,31 @@ window.App = Backbone.View.extend({
       if(poll){
           this.polls.create({
                                  question: poll,
-                                 pub_date: '00:00:00',
+                                 pub_date: this.polls.getCurrentDate(),         
                              });
           this.$('#question').val('');
       }
   }                                  
 });
 
-window.app = new App();
+
+window.Router = Backbone.Router.extend({
+    routes: {
+            '': 'polls',
+            'polls/:id/': 'detail'
+        },
+    navigate_to: function(model){
+            var path = (model && model.get('id') + '/') || '';
+            alert(path);
+            this.navigate(path, true);
+        },
+});
+
+window.app = window.app || {};
+app.router = new Router();
+app.polls = new PollList()
+app.list = new App({
+    el : $("#app"),
+    collection : app.polls,
+});
+app.list.bind('navigate', app.router.navigate_to, app.router);
